3285bad4b344d569c70ea75c63f29fae969db257
Mrt1ns <72765821+Mrt1ns@users.noreply.github.com>
2023-05-20 13:38:53 -0300

    Corrigindo o app.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação sendo conextada ao banco de dados")
+console.log("Aplicação contato sendo conextada ao banco de dados")
